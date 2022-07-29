3fba790624772cee0da57d8958aacf1d309253f2
imkuqin-zw <1141137429@qq.com>
2022-07-29 22:56:58 +0800

    feat: grpc md support header and trailer

--- pkg/server/grpc/md_interceptor.go
+++ pkg/server/grpc/md_interceptor.go
@@ -17,6 +17,7 @@ func MdUnaryServerInterceptor(ctx context.Context, req interface{},
 	info *grpc.UnaryServerInfo,
 	handler grpc.UnaryHandler,
 ) (interface{}, error) {
+	ctx = md.WithStreamContext(ctx)
 	if meta, ok := metadata.FromIncomingContext(ctx); ok {
 		if m, ok := md.FromInContext(ctx); ok {
 			ctx = md.WithInContext(ctx, md.Join(m, md.MD(meta)))
@@ -24,7 +25,18 @@ func MdUnaryServerInterceptor(ctx context.Context, req interface{},
 			ctx = md.WithInContext(ctx, md.MD(meta))
 		}
 	}
-	return handler(ctx, req)
+	res, resErr := handler(ctx, req)
+	if header, ok := md.FromHeaderCtx(ctx); ok {
+		if err := grpc.SetHeader(ctx, metadata.MD(header)); err != nil {
+			return nil, err
+		}
+	}
+	if trailer, ok := md.FromTrailerCtx(ctx); ok {
+		if err := grpc.SetTrailer(ctx, metadata.MD(trailer)); err != nil {
+			return nil, err
+		}
+	}
+	return res, resErr
 }
 
 type mdServerStream struct {
@@ -40,7 +52,7 @@ func MdStreamServerInterceptor(srv interface{}, stream grpc.ServerStream,
 	info *grpc.StreamServerInfo,
 	handler grpc.StreamHandler,
 ) error {
-	var ctx = stream.Context()
+	var ctx = md.WithStreamContext(stream.Context())
 	if meta, ok := metadata.FromIncomingContext(stream.Context()); ok {
 		if m, ok := md.FromInContext(ctx); ok {
 			ctx = md.WithInContext(ctx, md.Join(m, md.MD(meta)))
